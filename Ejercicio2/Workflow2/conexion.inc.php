ac137db6b10eca32f8145e36dcaa60e3a5a7ae6d
ijquenta <71706684+ijquenta@users.noreply.github.com>
2021-12-05 15:09:42 -0400

    Add files via upload

---
+++ Ejercicio2/Workflow2/conexion.inc.php
@@ -0,0 +1,5 @@
+<?php
+// conexion a la base de datos Flujo
+$conn = mysqli_connect("localhost","usuario324","123456"); // conectantado a phpMYSQL
+mysqli_select_db($conn,"workflowexamen");  // conectamos la base de datos bdivanexamen1
+?>
